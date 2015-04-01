e11e92d336d653416fd03e2dab7501a6bc1c0e28
Minoru NAKAMURA <nminoru1975@gmail.com>
2015-04-01 22:53:18 +0900

    Order-set aggregation のサンプルを修正

--- postgresql/sample/test-ordered-set-aggregate.sql
+++ postgresql/sample/test-ordered-set-aggregate.sql
@@ -6,6 +6,6 @@ CREATE TABLE agg_test (aggkey int4, i int4, t text);
 
 INSERT INTO agg_test (aggkey, i, t) SELECT i % 10, random() * 100, random() FROM generate_series(1, 1000) AS i;
 
-SELECT percentile_disc(0.5) WITH GROUP (ORDER BY aggkey) AS median FROM agg_test;
+SELECT mode() WITHIN GROUP (ORDER BY i) AS median, percentile_cont(0.5) WITHIN GROUP (ORDER BY i) AS median2, percentile_disc(0.5) WITHIN GROUP (ORDER BY i) AS median2 FROM agg_test;
 
 DROP TABLE agg_test;
