4ccb3f214658598296b95d6c04fd7e3eb1cadf59
Minoru NAKAMURA <nminoru1975@gmail.com>
2015-04-02 09:46:19 +0900

    Bugfix: timer_create() に対する timer_delete() が無かった。

--- postgresql/plan_profiler/plan_profiler.c
+++ postgresql/plan_profiler/plan_profiler.c
@@ -5,6 +5,7 @@
 #include "executor/executor.h"
 #include "fmgr.h"
 #include "nodes/nodes.h"
+#include "tcop/tcopprot.h"
 #include "utils/elog.h"
 
 PG_MODULE_MAGIC;
@@ -14,19 +15,18 @@ extern void _PG_init(void);
 static ExecutorRun_hook_type    executor_run_prev;
 static timer_t timerid;
 static struct itimerspec interval;
+static struct sigevent env;
 
 static void executor_run_routine(QueryDesc *queryDesc, ScanDirection direction, long count);
 static void plan_profiler_signal_handler(SIGNAL_ARGS);
 
-static unsigned int sampling_count;
+static volatile unsigned int sampling_count;
 static unsigned int histogram_data[T_LimitState - T_PlanState + 1];
 static void print_statics(void);
 
 void
 _PG_init(void)
 {
-	struct sigevent env;
-
 	executor_run_prev   = ExecutorRun_hook;
     ExecutorRun_hook    = executor_run_routine;
 
@@ -41,20 +41,25 @@ _PG_init(void)
 	interval.it_interval.tv_sec  = 0;
 	interval.it_interval.tv_nsec = 10 * 1000; /* 10 msec */
 	interval.it_value.tv_sec     = 0;
-	interval.it_value.tv_nsec    = 10 * 1000; /* 10 msec */
-
-	if (timer_create(CLOCK_REALTIME, &env, &timerid) != 0)
-		elog(ERROR, "timer_create()");
+	interval.it_value.tv_nsec    = 10 * 1000;
 }
 
 static void
 executor_run_routine(QueryDesc *queryDesc, ScanDirection direction, long count)
 {
+	int ret;
+
 	sampling_count = 0;
+
 	memset(&histogram_data, 0, sizeof(histogram_data));
 
-	if (timer_settime(timerid, 0, &interval, NULL) != 0)
-		elog(ERROR, "timer_settime()");
+	ret = timer_create(CLOCK_REALTIME, &env, &timerid);
+	if (ret != 0)
+		elog(ERROR, "timer_create() errno=%d", ret);
+
+	ret = timer_settime(timerid, 0, &interval, NULL);
+	if (ret != 0)
+		elog(ERROR, "timer_settime() errno=%d", ret);
 
 	PG_TRY();
 	{
@@ -65,13 +70,17 @@ executor_run_routine(QueryDesc *queryDesc, ScanDirection direction, long count)
 	}
 	PG_CATCH();
 	{
-		timer_settime(timerid, 0, NULL, NULL);
+		ret = timer_delete(timerid);
+		if (ret != 0)
+			elog(ERROR, "timer_delete() errno=%d", ret);
 
 		PG_RE_THROW();
 	}
 	PG_END_TRY();
 
-	timer_settime(timerid, 0, NULL, NULL);
+	ret = timer_delete(timerid);
+	if (ret != 0)
+		elog(ERROR, "timer_delete() errno=%d", ret);
 
 	if (sampling_count >= 100)
 		print_statics();
@@ -82,13 +91,18 @@ print_statics(void)
 {
 	NodeTag t;
 
+	printf("sampling_count = %d\n", sampling_count);
+	printf("STATEMENT: %s\n", debug_query_string);
+	
 	for (t = T_PlanState ; t <= T_LimitState ; t++)
 	{
 		if (histogram_data[t - T_PlanState] > 0)
 		{
-			printf("%3u: %3.2f%%\n", t, 1.0 * histogram_data[t - T_PlanState] / sampling_count);
+			printf("%3u: %3.2f%%\n", t, 100.0 * histogram_data[t - T_PlanState] / sampling_count);
 		}
 	}
+
+	printf("\n");
 }
 
 static void
@@ -102,5 +116,5 @@ plan_profiler_signal_handler(SIGNAL_ARGS)
 		histogram_data[tag - T_PlanState]++;
 	}
 
-	timer_settime(timerid, 0, &interval, NULL);
+	/* timer_settime(timerid, 0, &interval, NULL); */
 }
