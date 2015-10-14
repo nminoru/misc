8a10c0f94be352bb1d77db676e9d8523725785aa
Minoru NAKAMURA <nminoru1975@gmail.com>
2015-10-14 09:35:11 +0900

    Spark SQL のことはじめ

---
+++ spark/00readme.txt
@@ -0,0 +1,2 @@
+$ sbt package
+$ spark-submit --class "dbt3query1" --executor-memory 64g target/scala-2.11/dbt3query1_2.11-1.0.jar
---
+++ spark/dbt3query1.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
+
+object dbt3query1 {
+       def main(args: Array[String]) {
+       val conf = new SparkConf().setAppName("DBT3 Query1")
+       val sc = new SparkContext(conf)
+       val sqlContext = new SQLContext(sc)
+       import sqlContext.implicits._
+       val lineitemRDD = sqlContext.jdbc("jdbc:postgresql://localhost:5432/dbt3", "public.lineitem", "l_orderkey", 1, 60000000, 16)
+       lineitemRDD.registerTempTable("lineitem")
+       var res = sqlContext.sql("SELECT l_returnflag, l_linestatus, sum(l_quantity), sum(l_extendedprice), sum(l_extendedprice * (1 - l_discount)), sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)), avg(l_quantity), avg(l_extendedprice), avg(l_discount), count(*) FROM lineitem WHERE l_shipdate <= '1998-09-12' GROUP BY l_returnflag, l_linestatus ORDER BY l_returnflag, l_linestatus")
+       val start = System.currentTimeMillis
+       res.show()
+       println((System.currentTimeMillis - start) + "msec")
+  }
+}
