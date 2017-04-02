8cfe84b502029316b70232d80e64e449ab3e9898
Minoru NAKAMURA <nminoru1975@gmail.com>
2017-04-02 13:21:07 +0900

    テストを変更

--- postgresql/shm_test/shm_test.c
+++ postgresql/shm_test/shm_test.c
@@ -1,40 +1,24 @@
-#include <stdlib.h>
-#include <unistd.h>
-
 #include "postgres.h"
-#include "access/relscan.h"
-#include "access/sysattr.h"
-#include "access/xact.h"
-#include "catalog/pg_operator.h"
-#include "catalog/pg_type.h"
-#include "storage/ipc.h"
-#include "miscadmin.h"
 
-static const Size shmem_size = 64UL * 1024UL * 1024UL * 1024UL;
+#include "fmgr.h"
+#include "miscadmin.h"
+#include "storage/ipc.h"
+#include "storage/shmem.h"
+
+typedef struct
+{
+	int32  counter;
+
+} shared_memory_env;
 
 PG_MODULE_MAGIC;
 
 extern void _PG_init(void);
 
 static shmem_startup_hook_type shmem_startup_prev = NULL;
+static shared_memory_env * volatile shared_memory;
 
-static void *ShmemAddr;
-
-static void shmem_test_setup(void)
-{
-    void *address;
-    bool found;
-
-	if (shmem_startup_prev)
-		shmem_startup_prev();
-
-    address = ShmemInitStruct("shared memory", shmem_size, &found);
-    Assert(!address);
-
-	printf("shmem_test: pid=%u, shmem=%p\n", getpid(), address);
-
-	ShmemAddr = address;
-}
+static void shmem_test_setup(void);
 
 void
 _PG_init(void)
@@ -42,8 +26,46 @@ _PG_init(void)
     if (!process_shared_preload_libraries_in_progress)
         return;
 
-    RequestAddinShmemSpace(shmem_size);
+    RequestAddinShmemSpace(sizeof(shared_memory_env));
 
     shmem_startup_prev = shmem_startup_hook;
     shmem_startup_hook = shmem_test_setup;
+}
+
+
+static void
+shmem_test_setup(void)
+{
+    void *address;
+    bool found;
+
+	if (shmem_startup_prev)
+		shmem_startup_prev();
+
+    address = ShmemInitStruct("shared test memory", sizeof(shared_memory_env), &found);
+
+    Assert(!found);
+
+	shared_memory = (shared_memory_env *) address;
+}
+
+
+PG_FUNCTION_INFO_V1(show_shm_test);
+Datum
+show_shm_test(PG_FUNCTION_ARGS)
+{
+	int32 result;
+
+	if (!shared_memory)
+	{
+		void *address;
+		bool found;
+
+		address = ShmemInitStruct("shared test memory", sizeof(shared_memory_env), &found);
+		shared_memory = (shared_memory_env *) address;
+	}
+
+	result = ++shared_memory->counter;
+
+	PG_RETURN_INT32(result);
 }
