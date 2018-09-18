9407213d6ed6ad8675758ef23531db28f248ca9d
Minoru NAKAMURA <nminoru1975@gmail.com>
2018-09-19 03:13:11 +0900

    gitlab に API 経由でプロジェクトの一覧を表示

--- java/gitlab_client_test/src/main/java/com/example/ClientTest.java
+++ java/gitlab_client_test/src/main/java/com/example/ClientTest.java
@@ -1,7 +1,17 @@
 package com.example;
 
+<<<<<<< HEAD
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+=======
+import java.util.ArrayList;
+import java.util.List;
+>>>>>>> 855bfdc483cf3874d848c14e48b2c688537e20ac
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -26,6 +36,12 @@ public class ClientTest {
         return client.target("http://127.0.0.1:80/api/v4/");
     }
 
+<<<<<<< HEAD
+    /**
+     * リポジトリの一覧を表示
+     */
+=======
+>>>>>>> 855bfdc483cf3874d848c14e48b2c688537e20ac
     public void testProjects() {
         WebTarget target = generateTarget();
 
@@ -34,10 +50,36 @@ public class ClientTest {
             .request(MediaType.APPLICATION_JSON)
             .header("PRIVATE-TOKEN", "qGm5VgrCzA6PpqstUXtb")
             .get(String.class);
+<<<<<<< HEAD
+
+        System.out.println(response);
+
+        Jsonb jsonb = JsonbBuilder.create();
+
+        // Type hashMapType = new HashMap<String, Object>(){}.getClass().getGenericSuperclass();
+        // Type hashMapType = new HashMap<String, Object>(){}.getClass();
+        // Map<String, Object> result = jsonb.fromJson(response, hashMapType);
+        List repositories = jsonb.fromJson(response, List.class);
+
+        for (Object object : repositories) {
+            Map<String, Object> map = (Map<String, Object>) object;
+
+            for (String key : map.keySet()) {
+                System.out.print("key: " + key);
+                Object value = map.get(key);
+                if (value != null)
+                    System.out.print("  value: " + value.getClass().getName());
+                System.out.println();
+            }
+            
+            System.out.println();
+        }
+=======
         
         // assertTrue(response.success);
 
         System.out.println(response);
+>>>>>>> 855bfdc483cf3874d848c14e48b2c688537e20ac
     }
 }
 
