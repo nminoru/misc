6c593ca150b31e650b24ce33fcb88fe9b33c9c35
Minoru NAKAMURA <nminoru1975@gmail.com>
2018-09-20 00:01:21 +0900

    gitlab の各種 API の呼び出し

--- java/gitlab_client_test/src/main/java/com/example/ClientTest.java
+++ java/gitlab_client_test/src/main/java/com/example/ClientTest.java
@@ -1,10 +1,11 @@
 package com.example;
 
+import java.math.BigDecimal;
 import java.lang.reflect.Type;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.client.Client;
@@ -12,16 +13,49 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import static org.junit.Assert.*;
 
 public class ClientTest {
 
+    public static class Project {
+        public String name;
+        public int id;
+        public String http_url_to_repo;
+        
+        public Project() {
+        }
+
+        public Project(String name, int id, String http_url_to_repo) {
+            this.name = name;
+            this.id = id;
+            this.http_url_to_repo = http_url_to_repo;
+        }
+    }
+        
+
     public static void main(String[] args) {
         ClientTest c = new ClientTest();
 
-        c.testProjects();
+        c.createProject("abc");
+        c.createProject("def");
+        c.createProject("hij");
+        
+        List<Project> projects = c.listProjects();
+        
+        for (Project project : projects) {
+
+            System.out.println("name: " + project.name);
+            System.out.println("id: " + Integer.toString(project.id));
+            System.out.println("http_url_to_repo: " + project.http_url_to_repo);
+            System.out.println();
+
+            // c.listBranches(project.id);
+            
+            c.deleteProject(project.id);
+        }
     }
 
     public WebTarget generateTarget() {
@@ -34,7 +68,7 @@ public class ClientTest {
     /**
      * リポジトリの一覧を表示
      */
-    public void testProjects() {
+    public List<Project> listProjects() {
         WebTarget target = generateTarget();
 
         String response = target
@@ -43,7 +77,7 @@ public class ClientTest {
             .header("PRIVATE-TOKEN", "qGm5VgrCzA6PpqstUXtb")
             .get(String.class);
 
-        System.out.println(response);
+        // System.out.println(response);
 
         Jsonb jsonb = JsonbBuilder.create();
 
@@ -52,19 +86,88 @@ public class ClientTest {
         // Map<String, Object> result = jsonb.fromJson(response, hashMapType);
         List repositories = jsonb.fromJson(response, List.class);
 
+        List<Project> result = new ArrayList<>();
         for (Object object : repositories) {
             Map<String, Object> map = (Map<String, Object>) object;
 
-            for (String key : map.keySet()) {
-                System.out.print("key: " + key);
-                Object value = map.get(key);
-                if (value != null)
-                    System.out.print("  value: " + value.getClass().getName());
-                System.out.println();
-            }
+            String name = (String)map.get("name");
+            int id = ((BigDecimal)map.get("id")).intValue();
+            String http_url_to_repo = (String)map.get("http_url_to_repo");
             
+            result.add(new Project(name, id, http_url_to_repo));
+        }
+        
+        return result;
+    }
+
+    /**
+     * リポジトリの一覧を表示
+     */
+    public void createProject(String projectName) {
+        WebTarget target = generateTarget();
+
+        String response = target
+            .path("/projects")
+            .queryParam("name", projectName)
+            .queryParam("tag_list", "project")
+            .request(MediaType.APPLICATION_JSON)
+            .header("PRIVATE-TOKEN", "qGm5VgrCzA6PpqstUXtb")
+            .post(null, String.class);
+
+        // System.out.println(response);
+    }
+
+    public void listBranches(int projectId) {
+        WebTarget target = generateTarget();
+
+        String response = target
+            .path(String.format("/projects/%d/repository/branches", projectId))
+            .request(MediaType.APPLICATION_JSON)
+            .header("PRIVATE-TOKEN", "qGm5VgrCzA6PpqstUXtb")
+            .get(String.class);
+
+        System.out.println(response);
+
+        Jsonb jsonb = JsonbBuilder.create();
+
+        List branches = jsonb.fromJson(response, List.class);
+
+        List<Project> result = new ArrayList<>();
+        
+        for (Object object : branches) {
+            Map<String, Object> map = (Map<String, Object>) object;
+
+            printMap(map);
+        }        
+    }
+
+    /**
+     * リポジトリの一覧を表示
+     */
+    public void deleteProject(int projectId) {
+        WebTarget target = generateTarget();
+
+        Response response = target
+            .path(String.format("/projects/%d", projectId))
+            .request(MediaType.APPLICATION_JSON)
+            .header("PRIVATE-TOKEN", "qGm5VgrCzA6PpqstUXtb")
+            .delete();
+        
+        // System.out.println("status: " + response.getStatus());
+        
+        assertTrue(response.getStatusInfo().getStatusCode() == Response.Status.ACCEPTED.getStatusCode());
+    }
+
+    void printMap(Map<String, Object> map) {
+        for (String key : map.keySet()) {
+            System.out.print("key: " + key);
+            Object value = map.get(key);
+            if (value != null)
+                System.out.print("  value: " + value.getClass().getName());
             System.out.println();
         }
+
+        System.out.println();
     }
 }
 
