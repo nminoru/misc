ed034e146ff53d0a71dceb73cab2816f697a6d35
Minoru NAKAMURA <nminoru1975@gmail.com>
2019-04-16 23:41:55 +0900

    SMBJ の修正

--- java/smbj_test/src/main/java/jp/nminoru/smbj_test/Main.java
+++ java/smbj_test/src/main/java/jp/nminoru/smbj_test/Main.java
@@ -3,6 +3,7 @@ package jp.nminoru.smbj_test;
 import java.io.IOException;
 import com.hierynomus.msfscc.FileAttributes;
 import com.hierynomus.msfscc.fileinformation.FileIdBothDirectoryInformation;
+import com.hierynomus.protocol.commons.EnumWithValue;
 import com.hierynomus.smbj.SMBClient;
 import com.hierynomus.smbj.auth.AuthenticationContext;
 import com.hierynomus.smbj.connection.Connection;
@@ -12,22 +13,23 @@ import com.hierynomus.smbj.share.DiskShare;
 import org.apache.commons.codec.binary.Hex;
 
 public class Main {
-    final static String ServerName = "SERVER";
-    final static String DomainName = "DOMAIN";
-    final static String UserName   = "USERNAME";
-    final static String Password   = "PASSWORD";
-    final static String ShareName  = "SHARENAME";
     
     public static void main(String[] args) throws IOException {
-        SMBClient client = new SMBClient();
+        String serverName = getEnv("SMBJ_SERVER");
+        String domainName = getEnv("SMBJ_DOMAIN");
+        String userName   = getEnv("SMBJ_USERNAME");
+        String password   = getEnv("SMBJ_PASSWORD");
+        String shareName  = getEnv("SMBJ_SHARENAME");
 
-        try (Connection connection = client.connect(ServerName)) {
-            AuthenticationContext ac = new AuthenticationContext(UserName, Password.toCharArray(), DomainName);
+        SMBClient client = new SMBClient();
+        
+        try (Connection connection = client.connect(serverName)) {
+            AuthenticationContext ac = new AuthenticationContext(userName, password.toCharArray(), domainName);
             Session session = connection.authenticate(ac);
 
             // Connect to Share
-            try (DiskShare share = (DiskShare) session.connectShare(ShareName)) {
-                for (FileIdBothDirectoryInformation f : share.list("*", "*")) {
+            try (DiskShare share = (DiskShare) session.connectShare(shareName)) {
+                for (FileIdBothDirectoryInformation f : share.list("", "*")) {
                     long fileAttributes = f.getFileAttributes();
                     
                     System.out.println("File : " + f.getFileName());
@@ -37,14 +39,29 @@ public class Main {
                     System.out.println("\t" + f.getChangeTime());
                     System.out.println("\t" + f.getAllocationSize());
                     System.out.println("\t" + fileAttributes);
-                    
-                    if ((fileAttributes & FileAttributes.FILE_ATTRIBUTE_DIRECTORY.getValue()) != 0)
+
+                    if (EnumWithValue.EnumUtils.isSet(fileAttributes, FileAttributes.FILE_ATTRIBUTE_DIRECTORY))
                         System.out.println("\tDIRECTORY");
 
+                    if (EnumWithValue.EnumUtils.isSet(fileAttributes, FileAttributes.FILE_ATTRIBUTE_HIDDEN))
+                        System.out.println("\tHIDDEN");
+                    
                     System.out.println("\t" + f.getEaSize());
                     System.out.println("\t" +  new String(Hex.encodeHex(f.getFileId())));
                 }
             }
         }
     }
+
+    public static String getEnv(String name) {
+        String value = System.getenv(name);
+
+        if (value == null) {
+            System.err.println(name + " is not defiend");
+            System.exit(1);
+        }
+
+        return value;
+    }
+
 }
