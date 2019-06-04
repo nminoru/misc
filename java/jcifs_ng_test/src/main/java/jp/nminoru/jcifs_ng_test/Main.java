db87022749fc4d9676cd206acfe9fb80ec687dd3
Minoru NAKAMURA <nminoru1975@gmail.com>
2019-06-04 13:10:18 +0900

    HDFS で XATTR を使う

--- java/jcifs_ng_test/src/main/java/jp/nminoru/jcifs_ng_test/Main.java
+++ java/jcifs_ng_test/src/main/java/jp/nminoru/jcifs_ng_test/Main.java
@@ -2,16 +2,21 @@ package jp.nminoru.jcifs_ng_test;
 
 import java.net.MalformedURLException;
 import java.util.Properties;
+import javax.security.auth.Subject;
+import javax.security.auth.kerberos.KerberosPrincipal;
 import jcifs.CIFSContext;
 import jcifs.CIFSException;
 import jcifs.config.PropertyConfiguration;
 import jcifs.context.BaseContext;
+import jcifs.smb.Kerb5Authenticator;
 import jcifs.smb.NtlmPasswordAuthenticator;
 import jcifs.smb.SmbException;
 import jcifs.smb.SmbFile;
 
 
 public class Main {
+
+    static boolean useKerberos = true;
     
     public static void main(String[] args) throws CIFSException, MalformedURLException, SmbException {
         String url = getEnv("SMB_URL");
@@ -22,7 +27,17 @@ public class Main {
         Properties props = new Properties();
         CIFSContext context = new BaseContext(new PropertyConfiguration(props));
 
-        context.withCredentials(new NtlmPasswordAuthenticator(domainName, userName, password));
+        if (useKerberos) {
+            String realm = domainName.toUpperCase();
+            
+            // Kerb5Authenticator
+            KerberosPrincipal principal = new KerberosPrincipal(String.format("%s@%s", userName, realm), KerberosPrincipal.KRB_NT_PRINCIPAL);
+            Subject subject = null; // @todo 
+            context.withCredentials(new Kerb5Authenticator(subject, password, realm, null /* expire */));
+        } else {
+            // NtlmPasswordAuthenticator
+            context.withCredentials(new NtlmPasswordAuthenticator(domainName, userName, password));
+        }
 
         // "smb://server/sharename/"
         SmbFile smbFile = new SmbFile(url, context.withCredentials(new NtlmPasswordAuthenticator(domainName, userName, password)));
