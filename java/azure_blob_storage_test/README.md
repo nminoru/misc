1cdf1898203c9e437b14ad50e66b71475513c678
Minoru NAKAMURA <nminoru1975@gmail.com>
2019-11-30 20:09:10 +0900

    Azure Blob Storage でディレクトリのコピーを行うテスト

---
+++ java/azure_blob_storage_test/README.md
@@ -0,0 +1,16 @@
+# Azure Blob Storage へのアクセスのテスト
+
+```
+mvn compile
+```
+
+## 実行
+環境変数を設定する。
+
+- WASB_ACCOUNT_NAME
+- WASB_ACCOUNT_KEY
+- WASB_CONTAINER_NAME
+
+```
+mvn exec:java
+```
--- java/azure_blob_storage_test/src/main/java/jp/nminoru/azure_blob_storage_test/ClientTest.java
+++ java/azure_blob_storage_test/src/main/java/jp/nminoru/azure_blob_storage_test/ClientTest.java
@@ -15,6 +15,7 @@ import com.microsoft.azure.storage.blob.BlobContainerPublicAccessType;
 import com.microsoft.azure.storage.blob.BlobListingDetails;
 import com.microsoft.azure.storage.blob.BlobProperties;
 import com.microsoft.azure.storage.blob.BlobRequestOptions;
+import com.microsoft.azure.storage.blob.BlobType;
 import com.microsoft.azure.storage.blob.BlockEntry;
 import com.microsoft.azure.storage.blob.CloudAppendBlob;
 import com.microsoft.azure.storage.blob.CloudBlob;
@@ -74,39 +75,22 @@ public class ClientTest {
             return;
         }
 
-        CloudBlobDirectory directory;
-
-
         // ディレクトリの削除
-        for (ListBlobItem blobItem : container.listBlobs("foo/", true)) {
-            if (blobItem instanceof CloudBlob) {
-                CloudBlob cloudBlob = (CloudBlob)blobItem;
-                cloudBlob.delete();
-            }
-        }
-
-        directory = container.getDirectoryReference("foo/");
-        for (ListBlobItem blobItem : directory.listBlobs()) {
-            if (blobItem instanceof CloudBlob) {
-                CloudBlob cloudBlob = (CloudBlob)blobItem;
-
-                cloudBlob.deleteIfExists();
-            }
-        }
+        deleteFolder(container, "foo/");
 
         System.out.println("=== STEP 1 ===");
-        listFolder(container);
+        listFolder(container, "", false, false);
 
         HashMap<String, String> metadata = new HashMap<String, String>() {{put("hdi_isfolder", "true");}};
 
         // ディレクトの作成
-        CloudBlockBlob blob1;        
+        CloudBlockBlob blob1;
         blob1 = container.getBlockBlobReference("foo/bar/");
         blob1.setMetadata(metadata); // 必須ではない
         blob1.uploadFromByteArray(new byte[0], 0, 0);
 
         // ファイルの作成
-        CloudBlockBlob blob2;                
+        CloudBlockBlob blob2;
         blob2 = container.getBlockBlobReference("foo/test.txt");
         try (InputStream inputStream = ClientTest.class.getResourceAsStream("test.txt")) {
             blob2.upload(inputStream, -1);
@@ -120,41 +104,72 @@ public class ClientTest {
         }
 
         // ファイルの作成
-        CloudBlockBlob blob3;                        
-        blob3 = container.getBlockBlobReference("foo/bar/test.txt");
+        CloudBlockBlob blob3;
+        blob3 = container.getBlockBlobReference("foo/bar/baz/test.txt");
         try (InputStream inputStream = ClientTest.class.getResourceAsStream("test.txt")) {
             blob3.upload(inputStream, -1);
         }
 
         System.out.println("=== STEP 2 ===");
-        listFolder(container);
+        listFolder(container, "", false, false);
 
         System.out.println("=== STEP 3 ===");
-        getCloudBlob(container.getBlockBlobReference("foo/bar/test.txt"));
+        getCloudBlob(container.getBlockBlobReference("foo/bar/baz/test.txt"));
 
         // com.microsoft.azure.storage.StorageException
 
-        System.out.println("=== STEP 4 ===");
-        getCloudBlob(container.getBlobReferenceFromServer("foo/bar/test.txt"));
+        System.out.println("=== STEP 4.1 ===");
+        try {
+            getCloudBlob(container.getBlobReferenceFromServer("foo/bar/baz/test.txt"));
+        } catch (StorageException e) {
+            System.err.println("status = " + e.getHttpStatusCode());
+            throw e;
+        }
 
-        // ファイルのコピー        
+        System.out.println("=== STEP 4.2 ===");
+        try {
+            getCloudBlob(container.getBlobReferenceFromServer("foo/bar/"));
+        } catch (StorageException e) {
+            System.err.println("status = " + e.getHttpStatusCode());
+            throw e;
+        }
+
+        System.out.println("=== STEP 4.3 ===");
+        try {
+            getCloudBlob(container.getBlobReferenceFromServer("foo/bar/baz/"));
+        } catch (StorageException e) {
+            System.err.println("status = " + e.getHttpStatusCode());
+            // throw e;
+        }
+
+        // ファイルのコピー
         System.out.println("=== STEP 5 ===");
         copyCloudBlob(blob3, container.getBlockBlobReference("foo/test2.txt"));
 
-        // ディレクトリのコピー
         System.out.println("=== STEP 6 ===");
-        copyCloudBlob(blob1, container.getBlockBlobReference("foo/bar3/"));
-        listFolder(container); 
+        CloudBlockBlob blob4;
+        blob4 = container.getBlockBlobReference("foo/bar/");
+        copyCloudBlob(blob3, container.getBlockBlobReference("bar/"));
+
+        System.out.println("=== STEP 7 ===");
+        copyFolders(container, "foo/bar/", "bar4/");
+
+        System.out.println("=== STEP 8 ===");
+        listFolder(container, "新しいフォルダー/", false, false);
+
+        System.out.println("=== STEP 9 ===");
+        listFolder(container, "", true, false);
+
+        System.out.println("=== FINAL ===");
+        deleteFolder(container, "bar/");
+        deleteFolder(container, "foo/");
+        deleteFolder(container, "foo3/");
+        deleteFolder(container, "foo4/");
     }
 
-    private void listFolder(CloudBlobContainer container) {
-        System.out.println("listFolder");
-        _listFolder(container, "");
-    }
-
-    private void _listFolder(CloudBlobContainer container, String name) {        
+    private void listFolder(CloudBlobContainer container, String name, boolean useFlatBlobListing, boolean recursive) {
         // ディレクトリのリスティング
-        for (ListBlobItem blobItem : container.listBlobs(name, false, details, options, null)) {
+        for (ListBlobItem blobItem : container.listBlobs(name, useFlatBlobListing, details, options, null)) {
             System.out.println(blobItem.getUri());
 
             if (blobItem instanceof CloudBlob) {
@@ -164,7 +179,7 @@ public class ClientTest {
                 // getParent() CloudBlobDirectory
                 // StorageUri getSnapshotQualifiedStorageUri()
 
-                System.out.println("\t" + cloudBlob.getName());
+                System.out.println("BLOB: " + cloudBlob.getName());
                 System.out.println("\t" + properties.getLength());
                 System.out.println("\t" + properties.getCreatedTime());
                 System.out.println("\t" + properties.getLastModified());
@@ -172,7 +187,54 @@ public class ClientTest {
                 // BlobProperties properties = blobItem.getProperties()
                 CloudBlobDirectory cloudBlobDirectory = (CloudBlobDirectory)blobItem;
 
-                _listFolder(container, cloudBlobDirectory.getPrefix());
+                System.out.println("VDIR: " + cloudBlobDirectory.getPrefix());
+
+                if (recursive)
+                    listFolder(container, cloudBlobDirectory.getPrefix(), useFlatBlobListing, recursive);
+            }
+        }
+    }
+
+    private void copyFolders(CloudBlobContainer container, String src, String destBase) throws InterruptedException, URISyntaxException, StorageException {
+        _copyFolders(container, src, src.length(), destBase);
+    }
+
+    private void _copyFolders(CloudBlobContainer container, String src, int srcLength, String destBase) throws InterruptedException, URISyntaxException, StorageException {
+        for (ListBlobItem blobItem : container.listBlobs(src, true)) {
+            if (blobItem instanceof CloudBlob) {
+                CloudBlob child = (CloudBlob)blobItem;
+
+                String name = child.getName();
+                String destName = destBase + name.substring(0, name.length() - srcLength);
+
+                copyCloudBlob(child, getCloudBlob(container, child.getProperties().getBlobType(), destName));
+            } else if (blobItem instanceof CloudBlobDirectory) {
+                CloudBlobDirectory child = (CloudBlobDirectory)blobItem;
+
+                String name = child.getPrefix();
+                _copyFolders(container, name, srcLength, destBase);
+            }
+        }
+    }
+
+    private void deleteFolder(CloudBlobContainer container, String folder) throws URISyntaxException, StorageException {
+        for (ListBlobItem blobItem : container.listBlobs(folder, true)) {
+            if (blobItem instanceof CloudBlob) {
+                CloudBlob cloudBlob = (CloudBlob)blobItem;
+                cloudBlob.delete();
+            } else if (blobItem instanceof CloudBlobDirectory) {
+                CloudBlobDirectory cloudBlobDirectory = (CloudBlobDirectory)blobItem;
+                deleteFolder(container, cloudBlobDirectory.getPrefix());
+            }
+        }
+
+        CloudBlobDirectory directory;
+        directory = container.getDirectoryReference(folder);
+        for (ListBlobItem blobItem : directory.listBlobs()) {
+            if (blobItem instanceof CloudBlob) {
+                CloudBlob cloudBlob = (CloudBlob)blobItem;
+
+                cloudBlob.deleteIfExists();
             }
         }
     }
@@ -198,9 +260,22 @@ public class ClientTest {
         String copyId = destBlob.startCopy(srcBlob.getUri());
         // abortCopy(copyId);
         CopyState copyState = destBlob.getCopyState();
-        
+
         while (copyState.getStatus().equals(CopyStatus.PENDING)) {
             Thread.sleep(1000);
+        }
+    }
+
+    private CloudBlob getCloudBlob(CloudBlobContainer container, BlobType blobType, String path) throws URISyntaxException, StorageException {
+        switch (blobType) {
+        case APPEND_BLOB:
+            return container.getAppendBlobReference(path);
+        case BLOCK_BLOB:
+            return container.getBlockBlobReference(path);
+        case PAGE_BLOB:
+            return container.getPageBlobReference(path);
+        default:
+            throw new EnumConstantNotPresentException(blobType.getClass(), blobType.toString());
         }
     }
 }
