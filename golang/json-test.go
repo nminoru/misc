d34b6ee6783c9492312c4e5ba91a54fb113f1605
Minoru NAKAMURA <nminoru1975@gmail.com>
2020-01-16 19:42:18 +0900

    Go の勉強

--- golang/json-test.go
+++ golang/json-test.go
@@ -11,16 +11,21 @@ type Book struct {
 	FileName         string    `json:"fileName"`
 }
 
+type BookAux struct {
+	Title            string    `json:"title"`
+}
+
 type BookShelf struct {
 	Count int `json:"count"`
 	Books []Book `json:"books"`
+    Aux   *BookAux `json:"aux"`
     Time  int64
 }
 
 func main() {
     var bookShelf BookShelf
 
-    bytes := []byte(`{"count":100,"books":[{"guid":"58c879c5-3183-4033-9789-bddbd58805b0","fileName":"foo.txt"},{"guid":"a59237f6-c084-43d8-8f13-72c4892b1048","fileName":"bar.txt"}]}`)
+    bytes := []byte(`{"count":100,"books":[{"guid":"58c879c5-3183-4033-9789-bddbd58805b0","fileName":"foo.txt"},{"guid":"a59237f6-c084-43d8-8f13-72c4892b1048","fileName":"bar.txt"}], "aux":{"title":"TITLE"}}`)
     
     if err := json.Unmarshal(bytes, &bookShelf); err != nil {
         log.Fatal(err)
@@ -33,6 +38,10 @@ func main() {
         fmt.Printf("[%d]\n", i);
         fmt.Println("\t" + book.GUID);
         fmt.Println("\t" + book.FileName);
+    }
+
+    if bookShelf.Aux != nil {
+        fmt.Println(bookShelf.Aux.Title)
     }
     
     fmt.Println(bookShelf.Time)
---
+++ golang/string-test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    name := "abc"
+
+    fmt.Println(name[1:])    
+    fmt.Println(name[2:])
+    fmt.Println(name[3:])
+}
